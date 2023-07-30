d53b13d4532d67a52bff7465fe1e06660aff4156
= <73474063+NotoriusNoLove@users.noreply.github.com>
2023-07-30 17:03:08 +0300

    fixed

--- way.py
+++ way.py
@@ -6,7 +6,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from decimal import Decimal
 
-global count, last
+global last
 count = 0
 last = ""
 
@@ -54,7 +54,7 @@ async def other(message: Message):
         return
     print(text, last)
     try:
-        chat_id[message.chat.id] += Decimal(text)
+        chat_id[message.chat.id] += text
         last = f"-{text}"
     except Exception as e:
         print(e)
@@ -66,9 +66,8 @@ async def other(message: Message):
 
 async def cancel(message: Message):
     global last
-    chat_id[message.chat.id] = eval(
-        f"{Decimal(str(chat_id[message.chat.id]))}{last}")
-    chat_id[message.chat.id] = Decimal(str(chat_id[message.chat.id]))
+    chat_id[message.chat.id] = Decimal(str(eval(
+        f"{Decimal(str(chat_id[message.chat.id]))}{last}")))
     print(chat_id[message.chat.id], type(chat_id[message.chat.id]))
     await message.answer(f"Число изменено на {last}! текущее число = {chat_id[message.chat.id]}")
 
